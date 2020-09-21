218de0de26f94d9229d564f029b7524e12ee8f2b
Alexandre.Bliard <alexandre.bliard@le-campus-numerique.fr>
2020-09-21 10:23:10 +0200

    create WelcomeTest

---
+++ PHP_Alternance_don-en-ligne-master/tests/Feature/WelcomeTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
+
+class ProjectTest extends TestCase
+{
+
+    public function testHTTPProject()
+    {
+        $response = $this->get('/project');
+
+        $response->assertStatus(200);
+    }
+
+    public function testTagH1() {
+        $response = $this->get('/project');
+        $value = '<h1>Liste des projets</h1>';
+        $response
+            ->assertSee($value, false);
+    }
+
+    public function testTitleProject() {
+        $response = $this->get('/project');
+        $value = '';
+    }
+
+
+}
